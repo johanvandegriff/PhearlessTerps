684d26823c163d4c3ab8db57521bf44370c852b6
nsobti <34111327+nsobti@users.noreply.github.com>
2018-04-10 16:47:41 -0400

    Navigation almost working

--- PhearlessTerps/PhearlessTerps.ino
+++ PhearlessTerps/PhearlessTerps.ino
@@ -9,11 +9,11 @@
 
 #include <Servo.h>
 #include "Hardware.h"
-#include "Util.h"
+//#include "Util.h"
 #define SensorPin 0          //pH meter Analog output to Arduino Analog Input 0
 unsigned long int avgValue;  //Store the average value of the sensor feedback
 float b;
-int buf[10],temp;
+int buf[10], temp;
 #include "Adafruit_VL53L0X.h"
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
@@ -26,7 +26,7 @@ Adafruit_VL53L0X lox = Adafruit_VL53L0X();
     int rxPin
     int txPin
 */
-Enes100 enes("pHearless Terps", CHEMICAL, 7, 8, 9);
+Enes100 enes("pHearless Terps", CHEMICAL, 5, 8, 9);
 
 double dabs(double val) {
   if (val > 0) return val;
@@ -34,19 +34,19 @@ double dabs(double val) {
   return 0;
 }
 
-#define MAX_NUM_MOTORS 8
-#define MAX_NUM_SERVOS 8
+static inline double sgn(double val) {
+  if (val < 0) return -1;
+  if (val == 0) return 0;
+  return 1;
+}
+
+#define MAX_NUM_MOTORS 2
+#define MAX_NUM_SERVOS 4
 
 Motor* motors[MAX_NUM_MOTORS] = {
   // PORT           IN1|PWM|EN| FB | SF |BRAKE
-  /*0*/ new Motor(  2,  3, 4, NONE, NONE, true), //left motor
-  /*1*/ new Motor(  5,  7, 12, NONE, NONE, true), //right motor
-  /*2*/ new Motor(),
-  /*3*/ new Motor(),
-  /*4*/ new Motor(),
-  /*5*/ new Motor(),
-  /*6*/ new Motor(),
-  /*7*/ new Motor()
+  /*0*/ new Motor(  5,  7, 12, NONE, NONE, true), //left motor
+  /*1*/ new Motor(  2,  3, 4, NONE, NONE, true), //right motor
 };
 
 
@@ -61,10 +61,6 @@ ServoControl* servos[MAX_NUM_SERVOS] = {
   /*1*/ new ServoControl(10                   ), //collection syringe servo
   /*2*/ new ServoControl(13                   ), //arm servo
   /*3*/ new ServoControl(11                   ), //lidar servo
-  /*4*/ new ServoControl(                     ),
-  /*5*/ new ServoControl(                     ),
-  /*6*/ new ServoControl(                     ),
-  /*7*/ new ServoControl(                     )
 };
 
 //call this from the setup() function to speed up analogRead
@@ -76,7 +72,7 @@ void EnableFastAnalogRead() {
 }
 
 void setup() {
-  
+
   EnableFastAnalogRead(); //enable fast analog reading
 
   for (uint8_t i = 0; i < MAX_NUM_SERVOS; i++) {
@@ -88,74 +84,27 @@ void setup() {
 
   lox.begin();
 
-
-  while (!enes.retrieveDestination()) 
+  while (!enes.updateLocation())
   {
     enes.println("Unable to retrieve location");
   }
+
+
+  enes.print("osv: ");
+  enes.print(enes.location.x);
+  enes.print(",");
+  enes.println(enes.location.y);
+
+  while (!enes.retrieveDestination())
+  {
+    enes.println("Unable to retrieve destination");
+  }
+
 
   enes.print("My destination is at ");
   enes.print(enes.destination.x);
   enes.print(",");
   enes.println(enes.destination.y);
-
-//  Serial.begin(9600);
-//  
-//  double targetHeading = PI / 2.0;
-//
-//  double TURN_GAIN = 0.2;
-//  double TURN_DEADZONE = 0.01;
-//  double TURN_MIN_SPEED = 0.05;
-//  double TURN_MAX_SPEED = 1;
-//  Vector2D targetHeadingVector = Vector2D(1, Angle::fromRadians(targetHeading));
-//  Vector2D directionVector = Vector2D(1, Angle::fromRadians(enes.location.theta));
-//
-//  //find the "signed angular separation", the magnitude and direction of the error
-//  double angleRadians = Vector2D::signedAngularSeparation(targetHeadingVector, directionVector).getRadians();
-//  Serial.print("signed angular separation: ");
-//  Serial.println(angleRadians);
-//
-//  //              This graph shows angle error vs. rotation correction
-//  //              ____________________________
-//  //              | correction.       ____   |
-//  //              |           .      /       |
-//  //              |           .   __/        |
-//  //              | ........__.__|.......... |
-//  //              |      __|  .     error    |
-//  //              |     /     .              |
-//  //              | ___/      .              |
-//  //              |__________________________|
-//  //
-//  //              The following code creates this graph:
-//
-//
-//  Serial.print("angleRadians: ");
-//  Serial.println(angleRadians);
-//
-//  //scale the signedAngularSeparation by a constant
-//  double rotationCorrection = TURN_GAIN * angleRadians;
-//  //                rotationCorrection = GYRO_PID.computeCorrection(0, angleRadians);
-//
-//  Serial.print("rotationCorrection before: ");
-//  Serial.println(rotationCorrection);
-//  Serial.println(TURN_DEADZONE);
-//  
-//  if (dabs(rotationCorrection) > TURN_MAX_SPEED) {
-//    //cap the rotationCorrection at +/- TURN_MAX_SPEED
-//    rotationCorrection = sgn(rotationCorrection) * TURN_MAX_SPEED;
-//  } else if (dabs(rotationCorrection) < TURN_DEADZONE) {
-//    //set it to 0 if it is in the deadzone
-//    rotationCorrection = 0;
-//  } else if (dabs(rotationCorrection) < TURN_MIN_SPEED) {
-//    //set it to the minimum if it is below
-//    rotationCorrection = sgn(rotationCorrection) * TURN_MIN_SPEED;
-//  }
-//
-//  Serial.print("rotationCorrection after: ");
-//  Serial.println(rotationCorrection);
-//
-//  motors[0]->setPower(255 * rotationCorrection);
-//  motors[1]->setPower(-255 * rotationCorrection);
 }
 
 uint64_t servoMask = 0;
@@ -210,252 +159,288 @@ char hexToChar(uint8_t h) {
 
 uint32_t loopTimer = 0;
 
-int task = 0;
+const uint8_t FOURFWD = 0;
+const uint8_t FOURTURN = 1;
+const uint8_t TURN1 = 2;
+const uint8_t TURN2 = 3;
+const uint8_t TURN3 = 4;
+const uint8_t STOP = 5;
+const uint8_t NAVTURN = 6;
+const uint8_t NAVFWD = 7;
+const uint8_t BROADCAST = 8;
 
-enum State {
-  FWD,
-  TURN1,
-  TURN2,
-  TURN3,
-  STOP,
-  NAVTURN,
-  NAVFWD,
-  BROADCAST
-};
-
-enum State state = FWD;
+uint8_t state = FOURFWD;
 
 uint32_t stateTimer = 0;
 int stateLength = 1000;
 
+const double TURN_GAIN = 0.8;
+const double TURN_DEADZONE = 0.15;
+const double TURN_MIN_SPEED = 0.7;
+const double TURN_MAX_SPEED = 1;
+const double closeToZero = 1.0e-3;
+
 boolean turn(double targetHeading) {
   boolean done = false;
-    double TURN_GAIN = 0.2;
-    double TURN_DEADZONE = 0.01;
-    double TURN_MIN_SPEED = 0.05;
-    double TURN_MAX_SPEED = 1;
-    Vector2D targetHeadingVector = Vector2D(1, Angle::fromRadians(targetHeading));
-    Vector2D directionVector = Vector2D(1, Angle::fromRadians(enes.location.theta));
+  double targetHeadingX = cos(targetHeading);
+  double targetHeadingY = sin(targetHeading);
 
-    //find the "signed angular separation", the magnitude and direction of the error
-    double angleRadians = Vector2D::signedAngularSeparation(targetHeadingVector, directionVector).getRadians();
-    enes.print("signed angular separation: ");
-    enes.println(angleRadians);
+  double directionX = cos(enes.location.theta);
+  double directionY = sin(enes.location.theta);
 
-    //              This graph shows angle error vs. rotation correction
-    //              ____________________________
-    //              | correction.       ____   |
-    //              |           .      /       |
-    //              |           .   __/        |
-    //              | ........__.__|.......... |
-    //              |      __|  .     error    |
-    //              |     /     .              |
-    //              | ___/      .              |
-    //              |__________________________|
-    //
-    //              The following code creates this graph:
+  //find the "signed angular separation", the magnitude and direction of the error
 
-    //scale the signedAngularSeparation by a constant
-    double rotationCorrection = TURN_GAIN * angleRadians;
-    //                rotationCorrection = GYRO_PID.computeCorrection(0, angleRadians);
+  //cross product of two 2D vectors in 3D space
+  double crossZ = targetHeadingX * directionY - targetHeadingY * directionX;
 
-    if (dabs(rotationCorrection) > TURN_MAX_SPEED) {
-      //cap the rotationCorrection at +/- TURN_MAX_SPEED
-      rotationCorrection = sgn(rotationCorrection) * TURN_MAX_SPEED;
-    } else if (dabs(rotationCorrection) < TURN_DEADZONE) {
-      //set it to 0 if it is in the deadzone
-      rotationCorrection = 0;
-      done = true;
-    } else if (dabs(rotationCorrection) < TURN_MIN_SPEED) {
-      //set it to the minimum if it is below
-      rotationCorrection = sgn(rotationCorrection) * TURN_MIN_SPEED;
+  double angleRadians = 0;
+  // If the vectors are too closely aligned, then return zero for
+  // separation.
+  if (dabs(crossZ) >= closeToZero) {
+
+    // To get the angle:
+    // a dot b = a * b * cos(angle)
+    // so angle = acos[ (a dot b) / (a * b) ]
+    // Make sure a * b is not too close to zero 0
+    double lengths = sqrt(targetHeadingX * targetHeadingX + targetHeadingY * targetHeadingY) * sqrt(directionX * directionX + directionY * directionY);
+    if (lengths >= closeToZero) {
+      // this is really an error, but to keep the OSV from crashing
+      double dot = targetHeadingX * directionX + targetHeadingY * directionY;
+      angleRadians = sgn(crossZ) * acos(dot / lengths);
+
     }
+  }
+  enes.print("signed angular separation: ");
+  enes.println(angleRadians);
 
-    enes.print("rotationCorrection: ");
-    enes.println(rotationCorrection);
+  //              This graph shows angle error vs. rotation correction
+  //              ____________________________
+  //              | correction.       ____   |
+  //              |           .      /       |
+  //              |           .   __/        |
+  //              | ........__.__|.......... |
+  //              |      __|  .     error    |
+  //              |     /     .              |
+  //              | ___/      .              |
+  //              |__________________________|
+  //
+  //              The following code creates this graph:
 
-    motors[0]->setPower(255 * rotationCorrection);
-    motors[1]->setPower(-255 * rotationCorrection);
+  //scale the signedAngularSeparation by a constant
+  double rotationCorrection = TURN_GAIN * angleRadians;
+  //                rotationCorrection = GYRO_PID.computeCorrection(0, angleRadians);
 
-    return done;
+  if (dabs(rotationCorrection) > TURN_MAX_SPEED) {
+    //cap the rotationCorrection at +/- TURN_MAX_SPEED
+    rotationCorrection = sgn(rotationCorrection) * TURN_MAX_SPEED;
+  } else if (dabs(rotationCorrection) < TURN_DEADZONE) {
+    //set it to 0 if it is in the deadzone
+    rotationCorrection = 0;
+    done = true;
+  } else if (dabs(rotationCorrection) < TURN_MIN_SPEED) {
+    //set it to the minimum if it is below
+    rotationCorrection = sgn(rotationCorrection) * TURN_MIN_SPEED;
+  }
+
+  enes.print("rotationCorrection: ");
+  enes.println(rotationCorrection);
+
+  motors[0]->setPower(200 * rotationCorrection);
+  motors[1]->setPower(-200 * rotationCorrection);
+
+  return done;
 }
 
 void loop() {
 
- updateDevices(loopTimer);
-   VL53L0X_RangingMeasurementData_t measure;
-  
+  updateDevices(loopTimer);
+  VL53L0X_RangingMeasurementData_t measure;
+
   lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
   if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-   if (measure.RangeMilliMeter < 300){
-    double value=measure.RangeMilliMeter;
-      enes.print(" Object Detected Distance (mm): "); enes.println(value);
-   }
+    if (measure.RangeMilliMeter < 300) {
+      double value = measure.RangeMilliMeter;
+      enes.print("Object! (mm): "); enes.println(value);
+    }
   }
 
   //every 16 millis (on a different count than the motors) update OSV location
-  if (loopTimer % 16 == 8) 
+  //if (loopTimer % 16 == 8)
   enes.updateLocation();
+  enes.print("state: ");
+  enes.println(state);
+  if (state == FOURTURN)
+  {
 
-  if (state == FWD)
+
+    enes.print("osv (x, y, theta): (");
+    enes.print(enes.location.x);
+    enes.print(", ");
+    enes.print(enes.location.y);
+    enes.print(", ");
+    enes.print(enes.location.theta);
+    enes.println(")");
+
+    enes.print("dest (x, y, theta): (");
+    enes.print(enes.destination.x);
+    enes.print(", ");
+    enes.print(enes.destination.y);
+    enes.print(", ");
+    enes.print(enes.destination.theta);
+    enes.println(")");
+
+
+    double desiredTheta = 0;
+    if (turn(desiredTheta))
+    {
+      motors[0]->setPower(0);
+      motors[1]->setPower(0);
+      state = FOURFWD;
+    }
+
+  } else if (state == FOURFWD)
   {
-    
-      stateTimer = millis();
-      motors[0]->setPower(200);
-      motors[1]->setPower(200);
-      
-      if(enes.location.x <= 4) 
-      {
-        enes.println(enes.location.x);
-      }
-      else
-      {
-        motors[0]->setPower(0);
-        motors[1]->setPower(0);
-        state=STOP;
-      }
-    
-  }else if (state == TURN1)
+    stateTimer = millis();
+    motors[0]->setPower(200);
+    motors[1]->setPower(200);
+    double thetaError = dabs(enes.location.theta);
+    if (thetaError >= PI / 16)
+    {
+      state = FOURTURN;
+    } else if (enes.location.x <= 2)
+    {
+
+      enes.print("osv (x, y, theta): (");
+      enes.print(enes.location.x);
+      enes.print(", ");
+      enes.print(enes.location.y);
+      enes.print(", ");
+      enes.print(enes.location.theta);
+      enes.println(")");
+
+      enes.print("dest (x, y, theta): (");
+      enes.print(enes.destination.x);
+      enes.print(", ");
+      enes.print(enes.destination.y);
+      enes.print(", ");
+      enes.print(enes.destination.theta);
+      enes.println(")");
+
+
+      enes.print("thetaError: ");
+      enes.println(thetaError);
+
+
+    }
+    else
+    {
+      motors[0]->setPower(0);
+      motors[1]->setPower(0);
+      state = NAVTURN;
+    }
+  } else if (state == TURN1)
   {
-    if(turn(PI / 2.0)) 
+    if (turn(PI / 2.0))
     {
       state = TURN2;
     }
 
-  }else if (state == TURN2)
+  } else if (state == TURN2)
   {
-    if(turn(-PI / 2.0)) 
+    if (turn(PI))
     {
       state = TURN3;
     }
 
-  }else if (state == TURN3)
+  } else if (state == TURN3)
   {
-    if(turn(PI / 2.0)) 
+    if (turn(-PI / 2.0))
     {
       motors[0]->setPower(0);
       motors[1]->setPower(0);
-      state=STOP;
+      state = STOP;
     }
 
   }
-  else if(state==NAVTURN)
+  else if (state == NAVTURN)
   {
     enes.println(enes.location.x);
-    double x=enes.destination.x-enes.location.x;
-    double y = enes.destination.y-enes.location.y;
-    double desiredTheta= atan(y/x);
-    if(turn(desiredTheta)) 
+    double x = enes.destination.x - enes.location.x;
+    double y = enes.destination.y - enes.location.y;
+    double desiredTheta = atan(y / x);
+    if (turn(desiredTheta))
     {
       motors[0]->setPower(0);
       motors[1]->setPower(0);
-      state=NAVFWD;
+      state = STOP;
     }
-    
-  }else if(state==NAVFWD)
+
+  } else if (state == NAVFWD)
   {
-  
-      stateTimer = millis();
-      motors[0]->setPower(200);
-      motors[1]->setPower(200);
-    double x=enes.destination.x-enes.location.x;
-    double y = enes.destination.y-enes.location.y;
-    double desiredTheta= atan(y/x);
-    double thetaError=dabs(desiredTheta-enes.location.theta);
-    double distance= sqrt(x*x+ y*y);
-    if(thetaError>= PI/8)
+
+    stateTimer = millis();
+    motors[0]->setPower(200);
+    motors[1]->setPower(200);
+    double x = enes.destination.x - enes.location.x;
+    double y = enes.destination.y - enes.location.y;
+    double desiredTheta = atan(y / x);
+    double thetaError = dabs(desiredTheta - enes.location.theta);
+    double distance = sqrt(x * x + y * y);
+    if (thetaError >= PI / 16)
     {
-      state=NAVTURN;
-    }else if(distance >=.250) 
-      {
-        
-        enes.print("enes.location (x, y, theta): (");
-        enes.print(enes.location.x);
-        enes.print(", ");
-        enes.print(enes.location.y);
-        enes.print(", ");
-        enes.print(enes.location.theta);
-        enes.println(")");
-        
-        enes.print("enes.destination (x, y, theta): (");
-        enes.print(enes.destination.x);
-        enes.print(", ");
-        enes.print(enes.destination.y);
-        enes.print(", ");
-        enes.print(enes.destination.theta);
-        enes.println(")");
+      state = NAVTURN;
+    } else if (distance >= .250)
+    {
 
-        enes.print("x, y: ");
-        enes.print(x);
-        enes.print(", ");
-        enes.println(y);
+      enes.print("osv (x, y, theta): (");
+      enes.print(enes.location.x);
+      enes.print(", ");
+      enes.print(enes.location.y);
+      enes.print(", ");
+      enes.print(enes.location.theta);
+      enes.println(")");
 
-        
-        enes.print("desiredTheta");
-        enes.println(desiredTheta);
-        enes.print("thetaError");
-        enes.println(thetaError);
-        enes.print("distance");
-        enes.println(distance);
-        
-       
-      }
-      else
-      {
-        motors[0]->setPower(0);
-        motors[1]->setPower(0);
-        state=STOP;
-      }
-}
+      enes.print("dest (x, y, theta): (");
+      enes.print(enes.destination.x);
+      enes.print(", ");
+      enes.print(enes.destination.y);
+      enes.print(", ");
+      enes.print(enes.destination.theta);
+      enes.println(")");
+
+      enes.print("x, y: ");
+      enes.print(x);
+      enes.print(", ");
+      enes.println(y);
 
 
-if (state == STOP)
-{
+      enes.print("desiredTheta: ");
+      enes.println(desiredTheta);
+      enes.print("thetaError: ");
+      enes.println(thetaError);
+      enes.print("distance: ");
+      enes.println(distance);
+
+
+    }
+    else
+    {
+      motors[0]->setPower(0);
+      motors[1]->setPower(0);
+      state = STOP;
+    }
+  }
+
+
+  if (state == STOP)
+  {
     motors[0]->setPower(0);
     motors[1]->setPower(0);
-}
-
-//E
-if (state == BROADCAST) 
-{
-   for(int i=0;i<10;i++)       //Get 10 sample value from the sensor for smooth the value
-  { 
-    buf[i]=analogRead(SensorPin);
-    delay(10);
-  }
-  for(int i=0;i<9;i++)        //sort the analog from small to large
-  {
-    for(int j=i+1;j<10;j++)
-    {
-      if(buf[i]>buf[j])
-      {
-        temp=buf[i];
-        buf[i]=buf[j];
-        buf[j]=temp;
-      }
-    }
-  }
-  avgValue=0;
-  for(int i=2;i<8;i++)                      //take the average value of 6 center sample
-    avgValue+=buf[i];
-  float phValue=(float)avgValue*5.0/1024/6; //convert the analog into millivolt
-  phValue=3.5*phValue;                      //convert the millivolt into pH value
-    enes.print("    pH:");  
-    enes.print(phValue);
-    enes.println(" ");
-      enes.navigated();
-      // Transmit the initial pH of the pool
-      enes.baseObjective(phValue);
-      // Transmit the final pH of the pool
-      enes.baseObjective(7.0);
-      state=STOP;
-
-
-      
   }
 
 
 
-  
+
+
 }
